eb36124c22df7939aa4fd6e39797f8eaa8a76a52
Mário Garcia <mario.rggj@gmail.com>
2020-12-03 18:59:52 -0400

    Adds report repair implementation

--- src/1/report-repair.spec.ts
+++ src/1/report-repair.spec.ts
@@ -1,7 +1,14 @@
-import ReportRepair from './report-repair';
+import reportRepair from './report-repair';
 
 describe('Report Repair', () => {
-	it('Returns true', () => {
-		expect(ReportRepair()).toBe(true);
+
+	describe('Given valid expense report', () => {
+		const expenseReport = [1000, 5, 1020];
+
+		it('multiplies entries that sum up to 2020', () => {
+			expect(reportRepair(expenseReport)).toBe(1020000);
+		});
 	});
+
+
 });
--- src/1/report-repair.ts
+++ src/1/report-repair.ts
@@ -1,3 +1,19 @@
-export default function reportRepair() {
-	return true;
+export default function reportRepair(expenseReport: Array<number>): number {
+
+	for (let currentIndex = 0; currentIndex < expenseReport.length; currentIndex++) {
+		const currentEntry = expenseReport[currentIndex];
+		
+		for (let comparativeIndex = 0; comparativeIndex < expenseReport.length; comparativeIndex++) {
+			const 
+				comparativeEntry = expenseReport[comparativeIndex],
+				indexesAreDifferent = currentIndex !== comparativeIndex,
+				entriesSumUpTo2020 = currentEntry + comparativeEntry === 2020;
+
+			if (indexesAreDifferent && entriesSumUpTo2020) {
+				return currentEntry * comparativeEntry;
+			}
+		}
+	}
+
+	return 0;
 }
