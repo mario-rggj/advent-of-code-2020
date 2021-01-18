0932138ada5a6ff66c8a0d68517ffc663482ccd5
Mário Garcia <mario.rggj@gmail.com>
2021-01-17 20:32:03 -0400

    Refactors PassportParser and includes new feature to parse with validators.

--- src/4/tests/passport-parser.spec.ts
+++ src/4/tests/passport-parser.spec.ts
@@ -1,9 +1,57 @@
 import fs from 'fs';
 import PassportParser from '../utils/passport-parser';
+import BirthYearValidator from '../validators/birth-year-validator';
+import CountryIdValidator from '../validators/country-id-validator';
+import ExpirationYearValidator from '../validators/expiration-year-validator';
+import EyeColorValidator from '../validators/eye-color-validator';
+import HairColorValidator from '../validators/hair-color-validator';
+import HeightValidator from '../validators/height-validator';
+import IssueYearValidator from '../validators/issue-year-validator';
+import PassportIdValidator from '../validators/passport-id-validator';
 
 describe('PassportParser', () => {
 	describe('Given a valid passport batch input file', () => {
 		const passportBatchFilePath = './src/4/utils/passport-batch-file.txt';
+		const expectedPassports = [
+			{ 
+				eyeColor: 'gry', 
+				id: '860033327', 
+				expirationYear: '2020', 
+				hairColor: '#fffffd', 
+				birthYear: '1937',
+				issueYear: '2017',
+				countryId: '147',
+				height: '183cm'
+			},
+			{ 
+				eyeColor: 'amb', 
+				id: '028048884', 
+				expirationYear: '2023', 
+				hairColor: '#cfa07d', 
+				birthYear: '1929',
+				issueYear: '2013',
+				countryId: '350',
+				height: undefined
+			},
+			{ 
+				eyeColor: 'brn', 
+				id: '760753108', 
+				expirationYear: '2024', 
+				hairColor: '#ae17e1', 
+				birthYear: '1931',
+				issueYear: '2013',
+				countryId: undefined,
+				height: '179cm'
+			},
+			{ 
+				eyeColor: 'brn', 
+				id: '166559648', 
+				expirationYear: '2025', 
+				hairColor: '#cfa07d', 
+				issueYear: '2011',
+				height: '59in'
+			}
+		];
 
 		beforeEach(() => {
 			fs.writeFileSync(passportBatchFilePath,
@@ -20,6 +68,8 @@ describe('PassportParser', () => {
 				'\n' +
 				'hcl:#cfa07d eyr:2025 pid:166559648\n' +
 				'iyr:2011 ecl:brn hgt:59in\n');
+
+			
 		});
 
 		afterEach(() => {
@@ -29,46 +79,24 @@ describe('PassportParser', () => {
 		it('parses file to a passport array', () => {
 			const parsedPassports = PassportParser.fromBatchFile(passportBatchFilePath);
 
-			expect(parsedPassports).toEqual([
-				{ 
-					eyeColor: 'gry', 
-					id: '860033327', 
-					expirationYear: '2020', 
-					hairColor: '#fffffd', 
-					birthYear: '1937',
-					issueYear: '2017',
-					countryId: '147',
-					height: '183cm'
-				},
-				{ 
-					eyeColor: 'amb', 
-					id: '028048884', 
-					expirationYear: '2023', 
-					hairColor: '#cfa07d', 
-					birthYear: '1929',
-					issueYear: '2013',
-					countryId: '350',
-					height: undefined
-				},
-				{ 
-					eyeColor: 'brn', 
-					id: '760753108', 
-					expirationYear: '2024', 
-					hairColor: '#ae17e1', 
-					birthYear: '1931',
-					issueYear: '2013',
-					countryId: undefined,
-					height: '179cm'
-				},
-				{ 
-					eyeColor: 'brn', 
-					id: '166559648', 
-					expirationYear: '2025', 
-					hairColor: '#cfa07d', 
-					issueYear: '2011',
-					height: '59in'
-				}
-			]);
+			expect(parsedPassports).toEqual(expectedPassports);
+		});
+
+		it('parses file to a passport array with validators', () => {
+			const parsedPassports = PassportParser.fromBatchFileWithValidators(passportBatchFilePath);
+			const validators = [
+				new BirthYearValidator(),
+				new CountryIdValidator(),
+				new ExpirationYearValidator(),
+				new EyeColorValidator(),
+				new HairColorValidator(),
+				new HeightValidator(),
+				new IssueYearValidator(),
+				new PassportIdValidator()
+			];
+
+			const passportsWithValidators = expectedPassports.map(passport => ({ ...passport, validators }));
+			expect(parsedPassports).toEqual(passportsWithValidators);
 		});
 	});
 });
--- src/4/utils/passport-parser.ts
+++ src/4/utils/passport-parser.ts
@@ -1,9 +1,64 @@
 import Parser from '../../utils/parser';
 import Passport from '../models/passport';
+import BirthYearValidator from '../validators/birth-year-validator';
+import CountryIdValidator from '../validators/country-id-validator';
+import ExpirationYearValidator from '../validators/expiration-year-validator';
+import EyeColorValidator from '../validators/eye-color-validator';
+import HairColorValidator from '../validators/hair-color-validator';
+import HeightValidator from '../validators/height-validator';
+import IssueYearValidator from '../validators/issue-year-validator';
+import PassportIdValidator from '../validators/passport-id-validator';
+import Validator from '../validators/validator';
 
 export default class PassportParser {
 	static fromBatchFile(batchFilePath: string): Array<Passport> {
-		return Parser.readFileContent(batchFilePath)
+		const passportsWithParsedAttributes = PassportParser.parseFileContentIntoAttributes(batchFilePath);
+
+		return passportsWithParsedAttributes.map(passport => 
+			new Passport(
+				passport.birthYear,
+				passport.issueYear,
+				passport.expirationYear,
+				passport.height,
+				passport.hairColor,
+				passport.eyeColor,
+				passport.id,
+				passport.countryId,
+			)
+		);
+	}
+
+	static fromBatchFileWithValidators(batchFilePath: string): Array<Passport> {
+		const validators = [
+			new BirthYearValidator(),
+			new CountryIdValidator(),
+			new ExpirationYearValidator(),
+			new EyeColorValidator(),
+			new HairColorValidator(),
+			new HeightValidator(),
+			new IssueYearValidator(),
+			new PassportIdValidator()
+		];
+
+		const passportsWithParsedAttributes = PassportParser.parseFileContentIntoAttributes(batchFilePath);
+
+		return passportsWithParsedAttributes.map(passport => 
+			new Passport(
+				passport.birthYear,
+				passport.issueYear,
+				passport.expirationYear,
+				passport.height,
+				passport.hairColor,
+				passport.eyeColor,
+				passport.id,
+				passport.countryId,
+				validators
+			)
+		);
+	}
+
+	private static parseFileContentIntoAttributes(batchFilePath: string) {
+		const unparsedPassports = Parser.readFileContent(batchFilePath)
 			.split('\n\n')
 			.map(passport => {
 				return passport
@@ -12,34 +67,37 @@ export default class PassportParser {
 					.reduce((passports, passportPiece) => {
 						return passports.concat(passportPiece);
 					});
-			})
-			.map(passport => {
-				return passport.map(attribute => {
-					const [key, value] = attribute.split(':');
-					return { key, value };
-				});
-			})
-			.map(passport => {
-				const issueYear = PassportParser.findAttribute(passport, 'iyr');
-				const birthYear = PassportParser.findAttribute(passport, 'byr');
-				const expirationYear = PassportParser.findAttribute(passport, 'eyr');
-				const height = PassportParser.findAttribute(passport, 'hgt');
-				const hairColor = PassportParser.findAttribute(passport, 'hcl');
-				const eyeColor = PassportParser.findAttribute(passport, 'ecl');
-				const id = PassportParser.findAttribute(passport, 'pid');
-				const countryId = PassportParser.findAttribute(passport, 'cid');
-
-				return new Passport(
-					birthYear,
-					issueYear,
-					expirationYear,
-					height,
-					hairColor,
-					eyeColor,
-					id,
-					countryId
-				);
 			});
+
+		const passportsWithSplittedAttributes = unparsedPassports.map(passport => {
+			return passport.map(attribute => {
+				const [key, value] = attribute.split(':');
+				return { key, value };
+			});
+		});
+
+		const passportsWithParsedAttributes = passportsWithSplittedAttributes.map(passport => {
+			const issueYear = PassportParser.findAttribute(passport, 'iyr');
+			const birthYear = PassportParser.findAttribute(passport, 'byr');
+			const expirationYear = PassportParser.findAttribute(passport, 'eyr');
+			const height = PassportParser.findAttribute(passport, 'hgt');
+			const hairColor = PassportParser.findAttribute(passport, 'hcl');
+			const eyeColor = PassportParser.findAttribute(passport, 'ecl');
+			const id = PassportParser.findAttribute(passport, 'pid');
+			const countryId = PassportParser.findAttribute(passport, 'cid');
+
+			return {
+				birthYear,
+				issueYear,
+				expirationYear,
+				height,
+				hairColor,
+				eyeColor,
+				id,
+				countryId
+			};
+		});
+		return passportsWithParsedAttributes;
 	}
 
 	private static findAttribute(passport: Array<Record<string, string>>, attributeKey: string): string | undefined {
